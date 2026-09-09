264801ba4a6b931547ea7f5bd12c490d359747ed
rogeriobs83 <rogeriobs@hotmail.co.uk>
2026-09-09 07:46:52 +0100

    Refactor Vite config for cleaner export and proxy setup
    Refactor Vite configuration to simplify export and enhance proxy settings for development.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default {
+  plugins: [react(), tailwindcss()],
   server: {
-    host: true,        // permite acesso pela rede local
-    port: 5173,
-    proxy: {
-      '/signup': 'http://127.0.0.1:8000'
-    }        // porta padrão do Vite
+    host: true, // permite acesso pela rede local
+    port: 5173, // porta padrão do Vite
+    proxy: process.env.NODE_ENV === 'development'
+      ? {
+          '/signup': {
+            target: 'http://127.0.0.1:8000', // backend local
+            changeOrigin: true,
+            secure: false,
+          },
+        }
+      : {},
   },
-})
+}
